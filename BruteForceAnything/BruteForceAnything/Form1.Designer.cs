2d888440252c66d6e764e459a20ad68aa310ca54
Pronner <pronnerdev@gmail.com>
2022-12-25 18:48:43 +0400

    Add files via upload

---
+++ BruteForceAnything/BruteForceAnything/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BruteForceAnything
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Begin BruteForce (F3)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(12, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(332, 102);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop BruteForce (F3)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 323);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(332, 208);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Focus on this textbox after clicking Begin to test the bruteforce.";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox2.Location = new System.Drawing.Point(12, 25);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(332, 69);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox2_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 543);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Bruteforcer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ BruteForceAnything/BruteForceAnything/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BruteForceAnything
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool passwordFound = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            passwordFound = false;
+
+            string password = "";
+
+            char[] possibleCharacters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-~`{[}];'|\""?/>.<,".ToCharArray();
+
+            string currentAttempt = "";
+
+            while (!passwordFound)
+            {
+                currentAttempt = incrementString(currentAttempt, possibleCharacters);
+
+                if (currentAttempt == password)
+                {
+                    passwordFound = true;
+                }
+                else
+                {
+                    passwordFound = false;
+                    richTextBox1.Text += Environment.NewLine + "Attempting with password: " + currentAttempt;
+                    System.Windows.Forms.Clipboard.SetText(currentAttempt);
+                    SendKeys.Send("^{v}");
+                    SendKeys.SendWait("{ENTER}");
+                    SendKeys.Send("^{a}");
+                    SendKeys.Send("{BACKSPACE}");
+
+                }
+            }
+        }
+
+        static string incrementString(string str, char[] possibleCharacters)
+        {
+            char[] characters = str.ToCharArray();
+
+            bool incremented = false;
+
+            for (int i = characters.Length - 1; i >= 0; i--)
+            {
+                if (characters[i] == possibleCharacters.Last())
+                {
+                    characters[i] = possibleCharacters.First();
+                }
+                else
+                {
+                    characters[i] = possibleCharacters[Array.IndexOf(possibleCharacters, characters[i]) + 1];
+
+                    // Set the flag to true to stop the loop
+                    incremented = true;
+                    break;
+                }
+            }
+
+            if (!incremented)
+            {
+                characters = (possibleCharacters.First() + new string(characters)).ToCharArray();
+            }
+
+            return new string(characters);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            passwordFound = true;
+            MessageBox.Show("Bruteforce stopped.");
+        }
+
+        private void richTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (richTextBox2.Text != string.Empty)
+                {
+
+                    passwordFound = true;
+                    MessageBox.Show("Bruteforce works!");
+
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+
+                if (passwordFound == true)
+                {
+
+                    MessageBox.Show("Bruteforce started.");
+                    button1.PerformClick();
+
+                }
+                else
+                {
+
+                    passwordFound = true;
+                    MessageBox.Show("Bruteforce stopped.");
+
+                }
+
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+
+            }
+        }
+    }
+}
